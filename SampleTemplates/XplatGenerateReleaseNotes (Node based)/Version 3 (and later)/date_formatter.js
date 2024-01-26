028401636e8882f287c178df7ff92a21b8e981cb
huggymann <61777949+huggymann@users.noreply.github.com>
2024-01-26 10:00:04 +0100

    Update date_formatter.js
    syntax correction in "Usage" section

--- SampleTemplates/XplatGenerateReleaseNotes (Node based)/Version 3 (and later)/date_formatter.js
+++ SampleTemplates/XplatGenerateReleaseNotes (Node based)/Version 3 (and later)/date_formatter.js
@@ -2,7 +2,7 @@
 // Azure DevOps returns dates in the format "Mon Aug 09 2021 08:31:02 GMT+0000 (Coordinated Universal Time)"
 // This functions swaps them to a local format e.g."August 09, 2021 - 08:31"
 /* Usage
-    {{date_formatter buildDetails.startTime}
+    {{date_formatter buildDetails.startTime}}
 */
 
 module.exports = {date_formatter(theDate) {
